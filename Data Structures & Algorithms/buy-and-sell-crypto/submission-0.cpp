5157329ca698e96a96961000ec854b25f51a0757
Prahalad M <156995484+Prahalad10@users.noreply.github.com>
2026-07-29 23:11:21 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans=INT_MIN;
+        int mini=INT_MAX;
+        for(int i=0;i<prices.size();i++){
+            mini=min(mini,prices[i]);
+            int profit=prices[i]-mini;
+            ans=max(ans,profit);
+        }
+        return ans;
+    }
+};
