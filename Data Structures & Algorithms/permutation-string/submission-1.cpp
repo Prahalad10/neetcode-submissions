fd13979377822389d5c4b3aee62bff94a28fafe6
Prahalad M <156995484+Prahalad10@users.noreply.github.com>
2026-07-29 23:54:12 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char,int> mpp;
+        for(char c:s1) mpp[c]++;
+        int n=s1.size();
+        for(int i=0;i<s2.size();i++){
+            unordered_map<char,int> temp;
+            int sizew=1;
+            int ind=i;
+            while(sizew<=n && ind<s2.size()){
+                temp[s2[ind]]++;
+                sizew++;
+                ind++;
+            }
+            bool valid=true;
+            for(char c='a';c<='z';c++){
+                if(mpp[c]!=temp[c]){
+                    valid=false;
+                    break;
+                }
+            }
+            if(valid) return true;
+        }
+        return false;
+    }
+};
