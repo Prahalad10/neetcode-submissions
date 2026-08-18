3fe28510e18535482812026766e93470dec03621
Prahalad M <156995484+Prahalad10@users.noreply.github.com>
2026-08-18 15:02:30 +0530

    Add: boats-to-save-people - submission-2

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int l=0;
+        int r=people.size()-1;
+        int ans=0;
+        while(l<=r){
+            if(people[l]+people[r]<=limit) l++;
+            r--;
+            ans++;
+        }
+        return ans;
+    }
+};
