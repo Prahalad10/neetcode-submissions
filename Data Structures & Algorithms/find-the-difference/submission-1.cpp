9e6ae8a55fdd4d1ee6b2ab8e89daa0d16a409b1f
Prahalad M <156995484+Prahalad10@users.noreply.github.com>
2026-08-28 00:26:55 +0530

    Add: find-the-difference - submission-1

---
+++ Data Structures & Algorithms/find-the-difference/submission-1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum1=0;
+        int sum2=0;
+        for(int i=0;i<s.size();i++) sum1+=s[i];
+        for(int i=0;i<t.size();i++) sum2+=t[i];
+        return sum2-sum1;
+    }
+};
